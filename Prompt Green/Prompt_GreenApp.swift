965e755c31c8b2041aa8fc03560373dd5d90ff0d
Ayesha <ayeshazaker@gmail.com>
2025-01-01 21:02:51 -0500

    Initial Commit

---
+++ Prompt Green/Prompt_GreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Prompt_GreenApp.swift
+//  Prompt Green
+//
+//  Created by Ayesha Zaker on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct Prompt_GreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
