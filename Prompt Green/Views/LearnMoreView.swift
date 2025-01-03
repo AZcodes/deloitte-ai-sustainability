24fa13060b0b363acaf8b4dee604cfd9851032be
Ayesha <ayeshazaker@gmail.com>
2025-01-02 23:52:18 -0500

    New subpage

---
+++ Prompt Green/Views/LearnMoreView.swift
@@ -0,0 +1,21 @@
+//
+//  LearnMoreView.swift
+//  Prompt Green
+//
+//  Created by Ayesha Zaker on 1/2/25.
+//
+
+import SwiftUI
+
+struct LearnMoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .font(.system(size:100))
+    }
+}
+
+struct LearnMoreView_Previews: PreviewProvider {
+    static var previews: some View {
+        LearnMoreView()
+    }
+}
